fcf40fcff018721be9cb7a2b8c2da14146e22f93
dgsilveira <dgsilveira@sga.pucminas.br>
2022-09-21 14:54:03 -0300

    feat: adicionado Pessoa.cs

---
+++ PessoaAPi/Data/PessoaContext.cs
@@ -0,0 +1,13 @@
+﻿using PessoaAPi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PessoaAPi.Data
+{
+    public class PessoaContext : DbContext
+    {
+        public PessoaContext(DbContextOptions<PessoaContext> options) : base(options)
+        {
+        }
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
