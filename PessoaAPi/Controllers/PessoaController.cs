61353729d844de420a061b0cc385ca517842ae50
dgsilveira <dgsilveira@sga.pucminas.br>
2022-09-21 12:04:50 -0300

    feat: adicionado PessoaController.  Métodos AdicionaPessoa (Post), RecuperaPessoa (Get) e RecuperaPessoaPostId(Post)

---
+++ PessoaAPi/Controllers/PessoaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using PessoaAPi.Models;
+using System.Linq;
+
+namespace PessoaAPi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        private static List<Pessoa> pessoas = new List<Pessoa>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaPessoa(Pessoa pessoa)
+        {
+            pessoa.Id = id++;
+            pessoas.Add(pessoa);
+            return CreatedAtAction(nameof(RecuperaPessoaPorId), new { Id = pessoa.Id }, pessoa);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarPessoa()
+        {
+            return Ok(pessoas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaPessoaPorId(int id)
+        {
+            Pessoa pessoa = pessoas.FirstOrDefault(pessoa => pessoa.Id == id);
+            if(pessoa != null)
+            {
+                return Ok(pessoa);
+            }
+            return NotFound();
+        }
+    }
+}
